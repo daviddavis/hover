17b0490ffd34c1c0ce9f7160da319d29c289757d
michael j talarczyk <mjt@mijit.com>
2015-02-05 08:54:36 -0800

    Use TLSv1 not SSLv3 after Poodle vuln

--- lib/hover.rb
+++ lib/hover.rb
@@ -2,7 +2,7 @@ require "hover/version"
 require "hover/api"
 
 # rest-client doesn't allow use to ssl_version (as of 1.6.7)
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'SSLv3'
+OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'TLSv1'
 
 module Hover
   # Your code goes here...
