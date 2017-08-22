a5236fbda5fe9a3dbd4afce9c16ae1411a2d6947
Nick Gorbikoff <ngorbikoff@ajrinlt.com>
2017-08-22 18:11:10 -0500

    updating AUTH_URL, pumping TLS version, adding 200 auth code to authenticate method

--- lib/hover.rb
+++ lib/hover.rb
@@ -2,7 +2,7 @@ require "hover/version"
 require "hover/api"
 
 # rest-client doesn't allow use to ssl_version (as of 1.6.7)
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'TLSv1'
+OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = :TLSv1_2
 
 module Hover
   # Your code goes here...
